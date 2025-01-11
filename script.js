8f489e38f5013f97ec75467c3561d02b8780d7f5
Mbasval <120762752+Mbasval@users.noreply.github.com>
2025-01-11 00:14:00 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Add event listeners for the buttons
 document.getElementById('visual-btn').addEventListener('click', function() {
-    showPDF('assets/portfolio2.pdf');
+    showPDF('assets/portfolio2.pdf'); // Link to "Visual" portfolio
 });
 
 document.getElementById('ux-btn').addEventListener('click', function() {
-    showPDF('assets/portfolio.pdf');
+    showPDF('assets/portfolio.pdf'); // Link to "UX" portfolio
+});
+
+document.getElementById('back-home-btn').addEventListener('click', function() {
+    // Hide the PDF viewer and return to the welcome screen
+    document.getElementById('pdf-viewer').style.display = 'none';
+    document.getElementById('welcome').style.display = 'flex';
 });
 
 function showPDF(pdfUrl) {
